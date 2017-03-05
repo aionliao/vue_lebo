73c2b29392fc1b09f1ecaf05271879f17e4c7e78
lzkui2013 <1073869701@qq.com>
2017-03-05 19:17:13 +0800

    添加sub组件

---
+++ src/components/sub/sub.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+    <div :class="classes">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import prefix from '../../config/prefix.js';
+import oneOf from '../../utils/oneOf.js';
+
+const prefixCls = `${prefix}-sub`;
+export default {
+    props: {
+        type: {
+            type: String,
+            validator (value) {
+                return oneOf(value, ['top-left', 'top-right']);
+            }
+        }
+    },
+    computed: {
+        classes () {
+            return `${prefixCls} ${prefixCls}-${this.type}`;
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import "../../styles/custom";
+@sub-width: 16px;
+.@{css-prefix}-sub {
+    position: absolute;
+    left: -@sub-width / 2;
+    top: -@sub-width / 2;
+    height: @sub-width;
+	width: @sub-width;
+	line-height: @sub-width;
+	border: none;
+    border-radius: 50%;
+    background-color: @color-base;
+    color: #fff;
+	font-size: 12px;
+}
+</style>
