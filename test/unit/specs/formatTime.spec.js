5b8d919bb0e8c1d6526694c1940fd5f58564f685
vgsir <1073869701@qq.com>
2017-03-08 12:54:02 +0800

    修复了测试用例中时间问题，所有的时间戳跟时区有关

--- test/unit/specs/formatTime.spec.js
+++ test/unit/specs/formatTime.spec.js
@@ -1,7 +1,7 @@
 import formatTime from 'src/utils/formatTime.js';
-// var nowTime = new Date('2017-03-08 09:17:28').getTime();
 var nowTime = 1488935848000;
-console.log(nowTime);
+var timeOffset = (new Date().getTimezoneOffset() + 480) * 60 * 1000;
+nowTime += timeOffset;
 
 describe('formatTime 方法测试', () => {
 	it('ymdhm格式', () => {
