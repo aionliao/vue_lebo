0c286cb0b6077b4a6aaddf0916fe08a070166238
lzkui2013 <1073869701@qq.com>
2017-03-24 07:39:50 +0800

    倒计时方法

--- src/public/countDown.js
+++ src/public/countDown.js
@@ -1,3 +1,10 @@
-function counDown(timeSp) {
+import formatSolo from '../utils/formatSolo.js';
 
+function countDown (timeSp) {
+    let days = Math.floor(timeSp / 86400);
+    let hours = Math.floor((timeSp - 24 * days * 60 * 60) / 3600);
+    let minutes = Math.floor((timeSp - 24 * days * 60 * 60 - 3600 * hours) / 60);
+    let seconds = Math.floor(timeSp - 24 * days * 60 * 60 - 3600 * hours - 60 * minutes);
+    return days + '天' + formatSolo(hours) + ':' + formatSolo(minutes) + ':' + formatSolo(seconds);
 }
+export default countDown;
