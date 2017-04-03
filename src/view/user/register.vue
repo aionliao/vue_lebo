63b0704ad10b5a67f06186808f360e015d50401f
lzkui2013 <1073869701@qq.com>
2017-04-03 20:15:10 +0800

    更改登录和注册的位置

---
+++ src/view/user/register.vue
@@ -0,0 +1,58 @@
+<template>
+	<section class="login">
+		<headerSimple link="/" title="注册"></headerSimple>
+		<div class="login-input-area">
+			<inputArea iconType="people">
+				<input type="text" placeholder="4-12个字符或下划线，不能全部为数字" name="userName" regMsg="用户名4到16位" v-model="userNameVal" tag="用户名"  v-validate:userName/>
+			</inputArea>
+			<inputArea iconType="password">
+				<input type="password" placeholder="输入您的密码" name="password" v-model="passWordVal"  tag="密码" v-validate:password/>
+			</inputArea>
+			<inputArea iconType="password">
+				<input type="password" placeholder="请确认密码" name="password" v-model="passWordVal"  tag="密码" v-validate:password/>
+			</inputArea>
+		</div>
+		<div class="login-input-area">
+			<inputArea iconType="people" inputType="text" placeholder="4-12个字符或下划线，不能全部为数字"></inputArea>
+			<inputArea iconType="password" type="password" placeholder="请输入6-16个字符"></inputArea>
+			<inputArea iconType="password" type="password" placeholder="请确认密码"></inputArea>
+		</div>
+		<div class="login-input-area">
+			<inputArea iconType="mobilefill" inputType="text" placeholder="请输入手机号"></inputArea>
+			<inputArea iconType="yanzhengma" type="password" placeholder="请输入验证码"></inputArea>
+		</div>
+		<div class="login-btn-area">
+			<conWrap>
+				<i-button type="radius">注册</i-button>
+			</conWrap>
+		</div>
+		<conWrap>
+			<p class="text-right">
+				<router-link to="/forget" class="mid-size">忘记密码?</router-link>
+			</p>
+		</conWrap>
+		<linkArea></linkArea>
+	</section>
+</template>
+
+<script>
+	import headerSimple from '../../components/headers/headerSimple.vue';
+	import inputArea from '../../components/input/inputArea.vue';
+	import iButton from '../../components/iButton/iButton.vue';
+	import conWrap from '../../components/conWrap/conWrap.vue';
+	import linkArea from '../../components/linkArea/linkArea.vue';
+
+	export default {
+		components: {
+			headerSimple,
+			inputArea,
+			iButton,
+			conWrap,
+			linkArea
+		}
+	};
+</script>
+
+<style lang="less">
+	@import "./loginRelate.less";
+</style>
