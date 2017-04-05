b172cb3addbcfd6ee758c570ce3118d39add8488
vgsir <1073869701@qq.com>
2017-04-05 17:27:18 +0800

    添加商城首页

--- src/view/red/redIndex.vue
+++ src/view/red/redIndex.vue
@@ -1,8 +1,11 @@
 <template>
 	<sectionWrap mt="60" class="red-index">
 		<headerSimple link="/" title="商城"></headerSimple>
-		<template v-for="trendData in trendDataArr">
-			<redIndexItem :trendData="trendData"></redIndexItem>
+		<template v-for="redIndexData in redIndexDataArr">
+			<router-link :to="redIndexData.link" v-if="redIndexData.link">
+				<redIndexItem :redIndexData="redIndexData"></redIndexItem>
+			</router-link>
+			<redIndexItem :redIndexData="redIndexData" v-else></redIndexItem>
 		</template>
 	</sectionWrap>
 </template>
@@ -17,6 +20,40 @@
 			sectionWrap,
 			headerSimple,
 			redIndexItem
+		},
+		data () {
+			return {
+				redIndexDataArr: [
+					{
+						link: '',
+						type: 'left',
+						imgSrc: './static/img/red/red_03.png',
+						title: '购买乐米',
+						subTitleArr: ['乐米投注，精彩玩不停！', '带你变身大赢家！']
+					},
+					{
+						link: '',
+						type: 'right',
+						imgSrc: './static/img/red/red_07.png',
+						title: '乐米兑换',
+						subTitleArr: ['丰富商品兑换！', '数量有限先到先得！']
+					},
+					{
+						link: '',
+						type: 'left',
+						imgSrc: './static/img/red/red_10.png',
+						title: '购买红包',
+						subTitleArr: ['红包投注更优惠！', '全场红包9.7折起抢购！']
+					},
+					{
+						link: '',
+						type: 'right',
+						imgSrc: './static/img/red/red_12.png',
+						title: '更多商品',
+						subTitleArr: ['更多商品即将上线！', '敬请期待！']
+					}
+				]
+			};
 		}
 	};
 </script>
--- src/view/red/redIndexItem.vue
+++ src/view/red/redIndexItem.vue
@@ -1,11 +1,76 @@
 <template lang="html">
-
+	<div class="red-index-item">
+		<template v-if="redIndexData.type === 'left'">
+			<div class="red-index-left">
+				<img v-lazy="redIndexData.imgSrc">
+			</div>
+			<div class="red-index-right">
+				<h4>{{redIndexData.title}}</h4>
+				<template v-for="subTitle in redIndexData.subTitleArr">
+					<p>{{subTitle}}</p>
+				</template>
+			</div>
+		</template>
+		<template v-if="redIndexData.type === 'right'">
+			<div class="red-index-right align-right">
+				<h4>{{redIndexData.title}}</h4>
+				<template v-for="subTitle in redIndexData.subTitleArr">
+					<p>{{subTitle}}</p>
+				</template>
+			</div>
+			<div class="red-index-left">
+				<img v-lazy="redIndexData.imgSrc">
+			</div>
+		</template>
+	</div>
 </template>
 
 <script>
 export default {
+	props: {
+		redIndexData: [Object]
+	}
 };
 </script>
 
 <style lang="less">
+@import '../../styles/custom.less';
+@imgHei: 100px;
+.red-index-item {
+	background-color: #fff;
+	display: flex;
+	margin-bottom: @sub-module-space;
+	text-align: center;
+	align-items: center;
+	overflow: hidden;
+	white-space: nowrap;
+	.red-index-left {
+		width: 150px;
+
+	}
+	.red-index-right {
+		flex-grow: 1;
+		text-align: left;
+		margin-left: 20px;
+		margin-right: 0;
+		&.align-right {
+			margin-left: 0;
+			margin-right: 20px;
+			text-align: right;
+		}
+	}
+	h4 {
+		font-size: 16px;
+		color: @color-base;
+	}
+	p {
+		font-size: 14px;
+		line-height: 1.5;
+		color: @text-grap;
+	}
+	img {
+		width: @imgHei;
+		height: @imgHei;
+	}
+}
 </style>
